9788b5d24340544ae1c36a804d6bfc2b7f598f99
Aritra De <aritrade63@gmail.com>
2025-07-15 17:05:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { TexioApp } from '@/components/texio-app';
 import Preloader from '@/components/preloader';
