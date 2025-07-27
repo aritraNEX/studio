ee4f031dd24f8379bbaef6ff7cdf040927234ee9
Aritra De <aritrade63@gmail.com>
2025-07-27 03:49:18 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,13 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      // Don't watch the `patches` directory, which is created by patch-package
-      config.watchOptions.ignored = /patches/;
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
